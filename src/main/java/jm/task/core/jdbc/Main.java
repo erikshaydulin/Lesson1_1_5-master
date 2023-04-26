55c910800649ba0c5bba7fbffb1d86200ea29698
MikeStrat <erikshaydulin@gmail.com>
2023-04-27 02:13:32 +0500

    beta Hibernate 0.1b (Main bugfixes)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,16 +5,14 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl usi = new UserServiceImpl();
-//        usi.saveUser("Petr", "Petrov",(byte) 35);
         usi.createUsersTable();
-//        usi.saveUser("Nikita", "Salomandrov", (byte) 25);
-//        usi.saveUser("Zaur", "Tregulov", (byte) 29);
-//        usi.saveUser("Erik", "Shaydulin", (byte) 31);
-//        usi.saveUser("Elena", "Petrova", (byte) 19);
-//        System.out.println(usi.getAllUsers());
-//        usi.removeUserById(1);
-//        usi.cleanUsersTable();
-//        usi.dropUsersTable();
+        usi.saveUser("Nikita", "Salomandrov", (byte) 25);
+        usi.saveUser("Zaur", "Tregulov", (byte) 29);
+        usi.saveUser("Erik", "Shaydulin", (byte) 31);
+        usi.saveUser("Elena", "Petrova", (byte) 19);
+        System.out.println(usi.getAllUsers());
+        usi.cleanUsersTable();
+        usi.dropUsersTable();
 
     }
 }
